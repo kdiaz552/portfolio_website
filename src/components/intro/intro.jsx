bcfd8504cf436f110cdc131772d0bf832c1fabd4
kdiaz552 <kssy.dz@gmail.com>
2021-10-16 23:35:34 -0500

    new stuffs

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -1,23 +1,31 @@
 import "./intro.css"
+import Me from "../../img/me.png"
 
 const Intro = () => {
     return(
         <div className="i">
-            <div className="i-left"></div>
+            <div className="i-left">
                 <div className="i-left-wrapper">
-                    <h2 className="i-introduction">Hello, my name is </h2>
+                    <h2 className="i-intro">Hello, my name is</h2>
                     <h1 className="i-name">Kassandra Diaz</h1>
                     <div className="i-title">
-                        <div className="i-title-wrapper"></div>
-                        <div className="i-title-item">UTSA Alumni</div>
-                        <div className="i-title-item">UI/UX Designer</div>
-                        <div className="i-title-item">Web Developer</div>
-                        <div className="i-title-item">Resume</div>
-        
+                        <div className="i-title-wrapper">
+                            <div className="i-title-item">UTSA Alumni</div>
+                            <div className="i-title-item">B.S. in Computer Science</div>
+                            <div className="i-title-item">Web Developer</div>
+                            <div className="i-title-item">UI/UX Designer</div>
+                        </div>
                     </div>
+                    <p className="i-desc">
+                        Newly graduated from the University of Texas at San Antonio. Looking for entry level career opportunities. 
+                    </p>
                 </div>
-
-            <div className="i-right">right</div>
+                
+            </div>
+            <div className="i-right">
+                <div className="i-bg"></div>
+                <img src={Me} alt="" className="i-img" />
+            </div>
         </div>
     )
 }
