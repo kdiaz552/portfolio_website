bcfa6ce771027770ac09e5e381975535ba314575
kdiaz552 <kssy.dz@gmail.com>
2021-10-18 13:51:34 -0500

    intro page done

---
+++ src/components/about/About.jsx
@@ -0,0 +1,19 @@
+import "./about.css"
+
+const About = () => {
+    return(
+        <div className="a">
+            <div className="a-left">
+                <div className="a-card bg"></div>
+                <div className="a-card">
+                    <img src="https://media4.giphy.com/media/L8K62iTDkzGX6/giphy.gif?cid=ecf05e47t0kpox04l5zlak29984r4fmoau4v08s04jesosy4&rid=giphy.gif&ct=g" 
+                    alt="" 
+                    className="a-i" />
+                </div>
+            </div>
+            <div className="a-right">right</div>
+        </div>
+    )
+}
+
+export default About
